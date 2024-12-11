29098ad2d2fb8af3e94c519361fea1eb42c528d7
cristiantapiasjaramillo <cristiantapias75@gmail.com>
2024-12-10 19:59:16 -0500

    Create Capitulo4_Ejercicio7.py

---
+++ Capitulo4_Ejercicio7.py
@@ -0,0 +1,10 @@
+print("Ingrese número")
+A=int(input())
+print("Ingrese número")
+B=int(input())
+if A>B:
+    print(A, "es mayor que", B)
+elif A==B:
+    print(A, "es igual a", B)
+else:
+    print(A, "es menor que", B)
