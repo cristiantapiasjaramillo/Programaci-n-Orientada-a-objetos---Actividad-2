0a3e02dbd5eff920f8ede9b239f939d2db86f48e
cristiantapiasjaramillo <cristiantapias75@gmail.com>
2024-12-10 19:52:46 -0500

    Update Capitulo3_ejercicio19.py

--- Capitulo3_ejercicio19.py
+++ Capitulo3_ejercicio19.py
@@ -4,6 +4,6 @@ lado=float(input())
 perimetro_triangulo=3*lado
 altura_triangulo=((math.sqrt(3)*lado)/2)
 area_triangulo=lado*altura_triangulo
-print("El preimetro del triangulo es:", perimetro_triangulo)
-print("la altura del triangulo es:", altura_triangulo)
+print("El perimetro del triangulo es:", perimetro_triangulo)
+print("La altura del triangulo es:", altura_triangulo)
 print("El area del triangulo es:", area_triangulo)
